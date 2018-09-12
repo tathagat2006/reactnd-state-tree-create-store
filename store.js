17ac678833dbf0c47d3a8adb300b2e5f86029b30
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-09-12 22:33:56 +0530

    listen to the changes in the state
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- store.js
+++ store.js
@@ -9,12 +9,52 @@
 function createStore() {
 
     let state = {};
+    let listeners = [];
 
+    const subscribe = (listener) => {
+        listeners.push(listener)
+    }
     getState = () => {
         return state;
     }
 
     return {
-        getState
+        getState,
+        subscribe
     }
 }
+
+const store = createStore();
+store.getState();
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+})
+
+
+// $('button').click(function(){
+//     console.log("One");
+//     $('.firstChild').show(function(){
+//       console.log("Two");
+//       $('.childofChild').show();
+//     });
+//     console.log("Three");
+//   });
+
+// var request = require('request');
+// var app = require('express')();
+// var results;
+
+// function logRes(){
+//   console.log(results);
+// }
+
+// app.get('/storeData', function(req,res){
+//   readResult(logRes)
+// });
+
+// function readResult(callback){
+//   request('http://someroute.com/api', function(err, response, body){
+//     results=body;
+//     callback();
+//   });
+// }
