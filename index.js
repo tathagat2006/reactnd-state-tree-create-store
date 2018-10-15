b869894700c4940153890c05d45c7289ffa5a7b9
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-10-15 21:21:33 +0530

    using switch statements in reducer function
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- index.js
+++ index.js
@@ -54,3 +54,19 @@ function todos(action,state) {
         return state;
     }
 }
+
+//USING SWITCH CASE FOR ABOVE REDUCER FUNC
+
+function todos (state = [], action) {
+    switch(action.type) {
+      case 'ADD_TODO' :
+        return state.concat([action.todo]);
+      case 'REMOVE_TODO' :
+        return state.filter((todo) => todo.id !== action.id);
+      case 'TOGGLE_TODO' :
+        return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }));
+      default :
+        return state;
+    }
+  }
